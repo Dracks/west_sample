1f79daada1b6ab5bf32849703f04066d2b8c8986
jaume <jaume.singlavalls@gmail.com>
2023-11-09 22:34:59 +0100

    Added an extra route that returns the ip and the user agent

--- src/home_page/home.controller.v
+++ src/home_page/home.controller.v
@@ -20,3 +20,10 @@ fn (mut self HomePage) main() vweb.Result {
 	counter := self.service.count()
 	return self.html('<html><body><h1>Wellcome to West</h1><div>Hello world user ${counter}</div></body></html>')
 }
+
+['/ip']
+fn (mut self HomePage) get_ip() vweb.Result {
+	ip := self.ip()
+	ua := self.user_agent()
+	return self.html('<html><body><h1>Your Ip is: ${ip}</h1><div>User agent: ${ua}</div></body></html>')
+}
